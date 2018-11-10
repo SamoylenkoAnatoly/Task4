c100938cef241b4a939d8e1e87fa7cbdee5fbe36
Anatoly Samoylenko <homerjsimpsons1998@gmail.com>
2018-11-10 11:29:46 +0300

    Add files via upload

---
+++ src/slider.js
@@ -0,0 +1,26 @@
+var repeat;
+function reset(){
+    sec = 5;
+    document.getElementById("timer").childNodes[0].nodeValue = sec;
+}
+function init(){
+    sec = 5;
+    repeat = setInterval(tick, 1000);
+}
+
+function tick(){
+    if(sec === 1){
+        changePage('next');
+    }
+    sec--;
+    document.getElementById("timer").childNodes[0].nodeValue = sec;
+}
+
+function setPause(mode){
+    if(mode == 'stop'){
+        clearInterval(repeat);
+    }
+    if(mode == 'play'){
+        repeat = setInterval(tick, 1000);
+    }
+}
